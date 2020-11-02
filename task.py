24ac0d00941740d5946c724308b93164abc06bc1
repl.it user <replituser@example.com>
2020-11-02 15:19:12 +0000

    not working

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-#
+
 #Flight simulator. 
 #Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
 #The program should print out current orientation, and applied tilt correction.
@@ -7,18 +7,27 @@
 #Assume that plane orientation in every new simulation step is changing with random angle with gaussian distribution (the planes is experiencing "turbuence"). 
 # Hint: "random.gauss(0, 2*rate_of_correction)"
 #With every simulation step the orentation should be corrected, correction should be applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#Delete these comments before commit!
-#
-#Good luck.
+import random
+
+def drawGround():
+  for _ in range(20):
+    print("*",end = '')
+
+def drawPlaneWings(angle):
+  a = 
+
+def drawCorrection(angle):
+
+
+
+if __name__ == "__main__":
+  while True:
+    tempAngle = random.gauss(0, 2*180)
+    
+
+
+
+
+# I have no idea what to do :(
+# kind of hard to create smth impressive after 6h of classes in a row
+# do anything ;) make it run     
